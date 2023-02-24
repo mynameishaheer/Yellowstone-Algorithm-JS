ed9a8e434cb45972592caebd26c9b863c5ca4804
Shaheer Ahmed <94866222+mynameishaheer@users.noreply.github.com>
2023-02-24 17:50:37 +0500

    Used Vectors + UI Updates

--- Boid.js
+++ Boid.js
@@ -1,76 +1,89 @@
 class Boid {
-  constructor(x, y, dx, dy) {
-    this.x = x;
-    this.y = y;
-    this.dx = dx;
-    this.dy = dy;
-    this.visualRange = 50;
+  constructor(position, velocity) {
+    this.position = position;
+    this.velocity = velocity;
+    this.minSpeed = 3;
   }
 
-  update() {
-    this.cohesion();
-    this.seperation();
-    this.alignment();
-    this.limitSpeed();
+  update(cFactor, sFactor, aFactor, speedLimit, visualRange, minDistance) {
+    this.cohesion(cFactor, visualRange);
+    this.seperation(sFactor, minDistance);
+    this.alignment(aFactor, visualRange);
+
+    this.limitSpeed(speedLimit);
     this.keepInBounds();
-    this.x += this.dx;
-    this.y += this.dy;
+
+    this.position.add(this.velocity);
   }
 
   draw() {
-    let angle = atan2(this.dy, this.dx);
+    let angle = atan2(this.velocity.y, this.velocity.x);
     push();
-    translate(this.x, this.y);
+    translate(this.position.x, this.position.y);
     rotate(angle);
     fill(255);
     triangle(0, 0, -15, -5, -15, 5);
     pop();
+    this.drawStats();
   }
+
+  drawStats() {
+    textSize(16)
+    fill(150);
+    text("Number of Boids: " + numBoids, 10, 20);
+    text("Maximum Speed: " + speedLimitSlider.value(), 10, 40);
+    text("Visual Radius: " + visualRadiusSlider.value(), 10, 80);
+    text("Avoidance Radius: " + minDistanceSlider.value(), 10, 120)
+    text("Cohesion Factor: " + cSlider.value(), 10, 160)
+    text("Seperation Factor: " + sSlider.value(), 10, 200)
+    text("Alignment Factor: " + aSlider.value(), 10, 240)
+  }
+
 
   keepInBounds() {
-    const margin = 500;
-    const turnFactor = 0.1; //how fast does it turn
+    const margin = 250;
+    const turnFactor = 0.2; //how fast does it turn
 
-    if (this.x < margin) {
-      this.dx += turnFactor;
+    if (this.position.x < margin) {
+      this.velocity.x += turnFactor;
     }
-    if (this.x > windowWidth - margin) {
-      this.dx += -turnFactor;
+    if (this.position.x > windowWidth - margin) {
+      this.velocity.x -= turnFactor;
     }
-    if (this.y < margin) {
-      this.dy += turnFactor;
+    if (this.position.y < margin) {
+      this.velocity.y += turnFactor;
     }
-    if (this.y > windowHeight - margin) {
-      this.dy += -turnFactor;
+    if (this.position.y > windowHeight - margin) {
+      this.velocity.y -= turnFactor;
     }
   }
 
-  limitSpeed() {
-    const speedLimit = 5;
-    const speed = Math.sqrt(this.dx * this.dx + this.dy * this.dy); //magnitude
+  limitSpeed(speedLimit) {
+    const speed = this.velocity.mag();
+
     if (speed > speedLimit) {
-      this.dx = (this.dx / speed) * speedLimit; //normalize
-      this.dy = (this.dy / speed) * speedLimit; //normalize
+      this.velocity.x = (this.velocity.x / speed) * speedLimit; //normalize
+      this.velocity.y = (this.velocity.y / speed) * speedLimit; //normalize
+    }
+    if (speed < this.minSpeed) {
+      this.velocity.x = (this.velocity.x / speed) * this.minSpeed; //normalize
+      this.velocity.y = (this.velocity.y / speed) * this.minSpeed; //normalize
     }
   }
 
   calculateDistance(otherBoid) {
-    return Math.sqrt(
-      (this.x - otherBoid.x) * (this.x - otherBoid.x) +
-        (this.y - otherBoid.y) * (this.y - otherBoid.y)
-    );
+    return this.position.dist(otherBoid.position)
   }
 
-  cohesion() {
-    const cFactor = 0.005;
+  cohesion(cFactor, visualRange) {
     let centerX = 0;
     let centerY = 0;
     let nBoids = 0;
 
     for (let otherBoid of boids) {
-      if (this.calculateDistance(otherBoid) < this.visualRange) {
-        centerX = centerX + otherBoid.x;
-        centerY = centerY + otherBoid.y;
+      if (this.calculateDistance(otherBoid) < visualRange) {
+        centerX = centerX + otherBoid.position.x;
+        centerY = centerY + otherBoid.position.y;
         nBoids++;
       }
     }
@@ -79,14 +92,12 @@ class Boid {
       centerX = centerX / nBoids;
       centerY = centerY / nBoids;
 
-      this.dx += (centerX - this.x) * cFactor;
-      this.dy += (centerY - this.y) * cFactor;
+      this.velocity.x += (centerX - this.position.x) * cFactor;
+      this.velocity.y += (centerY - this.position.y) * cFactor;
     }
   }
 
-  seperation() {
-    const minDistance = 40;
-    const sFactor = 0.01;
+  seperation(sFactor, minDistance) {
 
     let moveX = 0;
     let moveY = 0;
@@ -94,27 +105,26 @@ class Boid {
     for (let otherBoid of boids) {
       if (otherBoid != this) {
         if (this.calculateDistance(otherBoid) < minDistance) {
-          moveX += this.x - otherBoid.x;
-          moveY += this.y - otherBoid.y;
+          moveX += this.position.x - otherBoid.position.x;
+          moveY += this.position.y - otherBoid.position.y;
         }
       }
     }
 
-    this.dx += moveX * sFactor;
-    this.dy += moveY * sFactor;
+    this.velocity.x += moveX * sFactor;
+    this.velocity.y += moveY * sFactor;
   }
 
-  alignment() {
-    const aFactor = 0.05;
+  alignment(aFactor, visualRange) {
 
     let avgDX = 0;
     let avgDY = 0;
     let nBoids = 0;
 
     for (let otherBoid of boids) {
-      if (this.calculateDistance(otherBoid) < this.visualRange) {
-        avgDX += otherBoid.dx;
-        avgDY += otherBoid.dy;
+      if (this.calculateDistance(otherBoid) < visualRange) {
+        avgDX += otherBoid.velocity.x;
+        avgDY += otherBoid.velocity.y;
         nBoids++;
       }
     }
@@ -123,8 +133,8 @@ class Boid {
       avgDX = avgDX / nBoids;
       avgDY = avgDY / nBoids;
 
-      this.dx += (avgDX - this.dx) * aFactor;
-      this.dy += (avgDY - this.dy) * aFactor;
+      this.velocity.x += (avgDX - this.velocity.x) * aFactor;
+      this.velocity.y += (avgDY - this.velocity.y) * aFactor;
     }
   }
 }
--- sketch.js
+++ sketch.js
@@ -1,23 +1,55 @@
 const numBoids = 50;
-
 var boids = [];
+
+let cSlider,
+  sSlider,
+  aSlider,
+  speedLimitSlider,
+  visualRadiusSlider,
+  minDistanceSlider;
 
 function setup() {
   createCanvas(windowWidth, windowHeight - 1);
+  cSlider = createSlider(0, 0.05, 0.005, 0.005);
+  cSlider.position(10, 165);
+  sSlider = createSlider(0, 0.1, 0.01, 0.01);
+  sSlider.position(10, 205);
+  aSlider = createSlider(0, 0.2, 0.09, 0.01);
+  aSlider.position(10, 245);
+  speedLimitSlider = createSlider(0, 20, 5, 0.5);
+  speedLimitSlider.position(10, 45);
+  visualRadiusSlider = createSlider(0, 300, 60, 1);
+  visualRadiusSlider.position(10, 85);
+  minDistanceSlider = createSlider(0, 150, 40, 1);
+  minDistanceSlider.position(10, 125);
+
+  var resetButton = createButton("Reset");
+  resetButton.position(10, 285);
+  resetButton.mousePressed(resetSketch);
+
   createBoids();
 }
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight - 1);
 }
 
+function resetSketch(){
+  cSlider.value(0.005);
+  sSlider.value(0.01);
+  aSlider.value(0.05);
+  speedLimitSlider.value(5);
+  visualRadiusSlider.value(60);
+  minDistanceSlider.value(40);
+  boids = [];
+  createBoids();
+}
+
 function createBoids() {
   for (let i = 0; i < numBoids; i++) {
     boids.push(
       new Boid(
-        random() * windowWidth,
-        random() * windowHeight - 1,
-        random(-1, 1),
-        random(-1, 1)
+        createVector(random() * windowWidth, random() * windowHeight - 1),
+        createVector(random(-1, 1), random(-1, 1))
       )
     );
   }
@@ -26,7 +58,14 @@ function createBoids() {
 function draw() {
   background(55);
   for (let boid of boids) {
-    boid.update();
+    boid.update(
+      cSlider.value(),
+      sSlider.value(),
+      aSlider.value(),
+      speedLimitSlider.value(),
+      visualRadiusSlider.value(),
+      minDistanceSlider.value()
+    );
     boid.draw();
   }
 }
